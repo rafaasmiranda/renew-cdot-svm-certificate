efc4d107bb259d4c913204d0994b56fef9cf5fab
Rafael Augusto Sena de Miranda <rafaasmiranda@users.noreply.github.com>
2017-04-13 19:56:24 -0300

    Begin block done. Added launch.json file for debugging. Remove $Controller parameter in order to simplify code. Might alter this later.

--- New-CdotSVMSelfSignedCertificate.ps1
+++ New-CdotSVMSelfSignedCertificate.ps1
@@ -8,13 +8,41 @@ param (
     [Parameter(Mandatory=$true, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
     [Alias('SVM')]
     [string[]]$Vserver,
-    [bool]$ExpiredOnly = $true,
-    [NetApp.Ontapi.Filer.C.NcController]$Controller
-    
+    [bool]$ExpiredOnly = $true
 )
 
 Begin {
+    Write-Debug "Checking if the NetApp DataONTAP module is installed"
 
+    if (!(Get-Module DataONTAP -ListAvailable)) {
+        throw "NetApp DataONTAP PowerShell module was not found. Please install the module and run the script again."
+    }
+    else {
+        Write-Debug "Importing DataONTAP module."
+        Import-Module DataONTAP
+    }
+
+    Write-Debug "Checking if it is needed to connect to a cluster"
+    
+    if (!$Global:CurrentNcController) {
+        try {
+            Write-Verbose "Initiating cluster connection"
+            $ClusterName = Read-Host -Prompt "Please inform the name or IP of the cluster"
+            if (Test-Connection -ComputerName $ClusterName -Count 2 -Quiet) {
+                Connect-NcController -Name $ClusterName -Credential (Get-Credential)
+            }
+            else {
+                throw "Failure connecting to $ClusterName. Not Reachable."
+            }
+            
+        }
+        catch {
+            throw "An error ocurred while attempting to connect to $ClusterName. $($Error[0].Exception.Message)"    
+        }
+    }
+    else {
+        Write-Verbose "Connected to Cluster $($Global:CurrentNcController.Name)"
+    }
 }
 
 Process {
@@ -22,5 +50,5 @@ Process {
 }
 
 End {
-    
+
 }
