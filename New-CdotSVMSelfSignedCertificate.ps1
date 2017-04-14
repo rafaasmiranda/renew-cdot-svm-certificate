60b36b66abf51abfecb2019ebc3a25f489e51f3e
Rafael Augusto Sena de Miranda <rafaasmiranda@users.noreply.github.com>
2017-04-13 21:09:14 -0300

    Add parameter for certificate options. Begun Process block implementation

--- New-CdotSVMSelfSignedCertificate.ps1
+++ New-CdotSVMSelfSignedCertificate.ps1
@@ -1,4 +1,5 @@
-﻿<#
+﻿#Requires -Version 3.0
+<#
 TODO: Help
 #>
 
@@ -7,7 +8,20 @@ param (
     [CmdletBinding(SupportsShouldProcess=$true, ConfirmImpact='Medium')]
     [Parameter(Mandatory=$true, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
     [Alias('SVM')]
-    [string[]]$Vserver
+    [string[]]$Vserver,
+    [Parameter(Mandatory=$true)]
+    [string]$EmailAddress,
+    [ValidatePattern("\[a-z]{2}")]
+    [string]$Country = ([System.Globalization.RegionInfo]((Get-Culture).Name)).TwoLetterISORegionName,
+    [string]$ExpireDays = 365,
+    [ValidateSet('sha1', 'sha256', 'md5')]
+    [string]$HashFunction = "SHA256",
+    [ValidateSet(512, 1024, 1536, 2048)]
+    [int]$Size = 2048,
+    [string]$State,
+    [string]$Organization,
+    [string]$OrganizationUnit,
+    [string]$Locality
 )
 
 Begin {
@@ -47,7 +61,32 @@ Begin {
 Process {
     # Date to compare with the certificate's date
     $ValidationDate = Get-Date
-
+    foreach ($VserverItem in $Vserver) {
+        # Get the certificate for the specified Vserver
+        $certificate = Get-NcSecurityCertificate -Vserver $VserverItem
+        # Check if the certificate is expired
+        if ($certificate.ExpirationDateDT -gt $ValidationDate) {
+            Write-Host "The certificate for $VserverItem is not expired. Nothing to do. :|"
+        }
+        else {
+            # Building hashtable parameters for New-NcSecurityCertificate
+            $NewCertParameters = @{
+                CommonName = $VserverItem
+                CertificateAuthority = $Global:CurrentNcController.Name
+                Type = 'server'
+                Vserver = $VserverItem
+                Country = $Country
+                ExpireDays = $ExpireDays
+                HashFunction = $HashFunction
+                State = $State
+                Locality = $Locality
+                Organization = $Organization
+                OrganizationUnit = $OrganizationUnit
+                EmailAddress = $EmailAddress
+                Size = $Size
+            }
+        }
+    }
 }
 
 End {
